2dd4038297434a3bf09f02d8b56c8bab95e3b2d3
croniccorey <corey.moyer@live.com>
2012-08-11 05:45:36 -0400

    Stabilize OC/UV

--- arch/arm/mach-msm/avs.h
+++ arch/arm/mach-msm/avs.h
@@ -34,12 +34,12 @@
 #define	USE_OVERCLOCKING	1
 #define	USE_EXTREMEOVERCLOCKING	1
 
-//#define VOLTAGE_MIN  875 /* mV */
 //#define VOLTAGE_MIN  925 /* mV */
-#define VOLTAGE_MIN  BRAVO_TPS65023_MIN_UV_MV /* mV */
+//#define VOLTAGE_MAX  1300 /* mV */
+#define VOLTAGE_MIN  BRAVO_TPS65023_MIN_UV_MV
 #define VOLTAGE_MAX  BRAVO_TPS65023_MAX_UV_MV
-#define	VOLTAGE_MAX_SAFE	VOLTAGE_MAX-50	// MAX voltage for safe frequencies (1300 mV)
-#define	VOLTAGE_MIN_START	1050	// Minimum value to start off with
+#define	VOLTAGE_MAX_SAFE  VOLTAGE_MAX-50 // MAX voltage for safe frequencies (1300 mV)
+#define	VOLTAGE_MIN_START  1050	// Minimum value to start off with
 #define VOLTAGE_STEP 25
 
 int __init avs_init(int (*set_vdd)(int), u32 freq_cnt, u32 freq_idx);
@@ -53,7 +53,7 @@ u32 avs_reset_delays(u32 avsdscr);
 u32 avs_get_avscsr(void);
 u32 avs_get_avsdscr(void);
 u32 avs_get_tscsr(void);
-void     avs_set_tscsr(u32 to_tscsr);
+void avs_set_tscsr(u32 to_tscsr);
 
 //#define AVSDEBUG(x...) pr_info("AVS: " x);
 #define AVSDEBUG(...)
